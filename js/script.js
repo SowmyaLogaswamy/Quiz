83cac76535059d8cddd5c4a03875e70fb41a8f0a
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-06-20 23:37:37 -0700

    Add basic elements in html and UI logic in js file

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//Business Logic
+
+//User Interface Logic
+$(document).ready(function() {
+ $("form#quizForm").submit(function(event) {
+   event.preventDefault();
+   //Get the input from the user
+  var question1 = $("input:radio[name='quest1']:checked").val();
+   var question2 = $("input:radio[name='quest2']:checked").val();
+   var question3 = $("input:radio[name='quest3']:checked").val();
+   if(question1 === "oa")
+   {
+     $("#para").text("1. Correct answer");
+   }
+   else if(question1 === "ob" || question2 === "oc")
+   {
+     $("#para").text("1. Sorry! Wrong answer");
+   }
+   if(question2 === "2a" || question2 === "2c")
+   {
+      $("#para").text("2. Sorry! Wrong answer");
+   }
+   else if(question2 === "2b")
+   {
+     $("#para").text("2. Correct answer");
+   }
+   if(question3 === "3a" || question3 === "3b")
+   {
+      $("#para").text("3. Sorry! Wrong answer");
+   }
+   else if(question3 === "3c")
+   {
+     $("#para").text("3. Correct answer");
+   }
+ });
+});
